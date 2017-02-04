66f419ec262fc4ea2c64c3e4d512ce28b3d3399a
Nuno Padriano <padriano@gmail.com>
2017-02-04 14:18:35 +0000

    Add new module catpeds-rest to expose CatPeds services via REST

---
+++ catpeds-rest/src/main/java/com/catpeds/Application.java
@@ -0,0 +1,19 @@
+package com.catpeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class for the standalone Spring Boot application execution.
+ *
+ * @author padriano
+ *
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
